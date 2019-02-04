98cf7e7f46279877cd6a45c4738c06d703473161
murakamiii <9khc727ru7@gmail.com>
2019-02-04 21:26:56 +0900

    Initial Commit

---
+++ WarikanExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WarikanExample
+//
+//  Created by MURAKAMI on 2019/02/04.
+//  Copyright © 2019 MURAKAMI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
